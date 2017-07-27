99d2383b8acaaa58122c4d4bad6af4d6223309ab
travisbrkr1234 <carlos.ochoa@infusionsoft.com>
2017-07-27 16:54:07 -0700

    AUTH-77_Infusionsoft_Id_Can_Be_Changed_To_An_Invalid_Email_Address
    Added regex from jquery.validate.js to User to match the front-end validation

--- cas/src/main/java/org/apereo/cas/infusionsoft/domain/User.java
+++ cas/src/main/java/org/apereo/cas/infusionsoft/domain/User.java
@@ -11,6 +11,7 @@ import org.joda.time.DateTime;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -73,7 +74,8 @@ public class User implements Serializable {
 
     @NotNull
     @Column(name = "username", unique = true, length = 120, nullable = false)
-    @Email(message = "{user.error.email.invalid}")
+    // This pattern was taken from jquery.validate.js to match how we validate on the front end.
+    @Pattern(message = "{user.error.email.invalid}", regexp = "^((([a-z]|\\d|[!#$%&'*+\\-/=?\\^_`{|}~]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])+(\\.([a-z]|\\d|[!#$%&'*+\\-/=?\\^_`{|}~]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])+)*)|((\\x22)((((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(([\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(\\\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF]))))*(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))@((([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.)+(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))$", flags = Pattern.Flag.CASE_INSENSITIVE)
     @Length(min = 8, max = 120, message = "{user.error.email.length}")
     @NotBlank(message = "{user.error.email.blank}")
     @Deprecated
